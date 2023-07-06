159b73b4ca37d04a17aaf20d88a2ee5769c675a1
Nurislam <maksatbekkov@icloud.com>
2023-07-06 13:18:51 +0600

    created model for user

---
+++ src/main/java/com/example/hw49/entity/User.java
@@ -0,0 +1,13 @@
+package com.example.hw49.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+    private boolean type;
+    private String password;
+}
