95d633cb03872dd89cd13cf9683176d9258fdbde
Nurislam <maksatbekkov@icloud.com>
2023-07-07 12:11:21 +0600

    created model for contact

---
+++ src/main/java/com/example/hw49/entity/Contact.java
@@ -0,0 +1,10 @@
+package com.example.hw49.entity;
+
+public class Contact {
+    private Long id;
+    private String telegram;
+    private String email;
+    private String number;
+    private String facebookLink;
+    private String linkedinLink;
+}
