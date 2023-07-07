11dd8d151c9de86bb5b6ca8ef3da069b6007245a
Nurislam <maksatbekkov@icloud.com>
2023-07-07 12:14:59 +0600

    created model for whoResponded

---
+++ src/main/java/com/example/hw49/entity/WhoResponded.java
@@ -0,0 +1,10 @@
+package com.example.hw49.entity;
+
+import java.time.LocalDateTime;
+
+public class WhoResponded {
+    private Long id;
+    private Employee whoResponded;
+    private Employer forWhatResponded;
+    private LocalDateTime dateTime;
+}
