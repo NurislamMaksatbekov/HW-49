b249c2337184c7b77833c001d58c8299cc1aecb4
Nurislam <maksatbekkov@icloud.com>
2023-07-07 12:03:53 +0600

    created model for employer

---
+++ src/main/java/com/example/hw49/entity/Employer.java
@@ -0,0 +1,11 @@
+package com.example.hw49.entity;
+
+public class Employer {
+    private Long id;
+    private String jobTitle;
+    private double salary;
+    private String jobDescription;
+    private String requiredExp;
+
+    private //TODO
+}
