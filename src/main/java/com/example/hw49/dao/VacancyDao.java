5aac14697bfa49ba91e560422ba7969cc48c9d87
Nurislam <maksatbekkov@icloud.com>
2023-07-18 16:55:52 +0600

    added methods for add, change and delete vacancy

--- src/main/java/com/example/hw49/dao/VacancyDao.java
+++ src/main/java/com/example/hw49/dao/VacancyDao.java
@@ -6,6 +6,8 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Component
@@ -30,4 +32,61 @@ public class VacancyDao {
                 "where category_id = ?";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Vacancy.class), categoryId);
     }
+
+    public void createVacancy(Vacancy vacancy){
+        String sql = "insert into vacancies(title, salary, author_email, job_description,  REQUIRED_MIN_EXPERIENCE, REQUIRED_MAX_EXPERIENCE, DATE_OF_POSTED, DATE_OF_UPDATED, active, CATEGORY_ID)" +
+                "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, vacancy.getTitle());
+            ps.setDouble(2, vacancy.getSalary());
+            ps.setString(3, vacancy.getAuthorEmail());
+            ps.setString(4, vacancy.getJobDescription());
+            ps.setInt(5, vacancy.getRequiredMinExp());
+            ps.setInt(6, vacancy.getRequiredMaxExp());
+            ps.setDate(7, Date.valueOf(vacancy.getDateOfPosted()));
+            ps.setDate(8, Date.valueOf(vacancy.getDateOfUpdated()));
+            ps.setBoolean(9, vacancy.isActive());
+            ps.setLong(10, vacancy.getCategoryId());
+
+            return ps;
+        });
+    }
+
+    public void changeVacancy(Vacancy vacancy) {
+        String sql = "UPDATE vacancies " +
+                "SET title = ?, salary = ?, author_email = ?, job_description = ?, " +
+                "    REQUIRED_MIN_EXPERIENCE = ?, REQUIRED_MAX_EXPERIENCE = ?, DATE_OF_POSTED = ?, " +
+                "    DATE_OF_UPDATED = ?, active = ?, CATEGORY_ID = ? " +
+                "WHERE ID = ?";
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, vacancy.getTitle());
+            ps.setDouble(2, vacancy.getSalary());
+            ps.setString(3, vacancy.getAuthorEmail());
+            ps.setString(4, vacancy.getJobDescription());
+            ps.setInt(5, vacancy.getRequiredMinExp());
+            ps.setInt(6, vacancy.getRequiredMaxExp());
+            ps.setDate(7, Date.valueOf(vacancy.getDateOfPosted()));
+            ps.setDate(8, Date.valueOf(vacancy.getDateOfUpdated()));
+            ps.setBoolean(9, vacancy.isActive());
+            ps.setLong(10, vacancy.getCategoryId());
+            ps.setLong(11, vacancy.getId());
+
+            return ps;
+        });
+    }
+
+    public void deleteVacancy(Long vacancyId) {
+        String sql = "DELETE FROM vacancies WHERE ID = ?";
+
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setLong(1, vacancyId);
+            return ps;
+        });
+    }
+
+
 }
--- src/main/java/com/example/hw49/service/VacancyService.java
+++ src/main/java/com/example/hw49/service/VacancyService.java
@@ -1,6 +1,7 @@
 package com.example.hw49.service;
 
 import com.example.hw49.dao.VacancyDao;
+import com.example.hw49.dto.VacancyDto;
 import com.example.hw49.entity.Vacancy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,19 +13,53 @@ import java.util.List;
 public class VacancyService {
     private final VacancyDao vacancyDao;
 
-    public List<Vacancy> getVacancyByResponds(String authorEmail){
+    public List<Vacancy> getVacancyByResponds(String authorEmail) {
         // Выборка вакансий на которые откликнулся пользователь
-       return vacancyDao.getVacancyByResponds(authorEmail);
+        return vacancyDao.getVacancyByResponds(authorEmail);
     }
 
-    public List<Vacancy> getAllVacancy(){
+    public List<Vacancy> getAllVacancy() {
         // Выборка всех вакансий
         return vacancyDao.getAllVacancy();
     }
 
-    public List<Vacancy> getVacancyByCategory(Long categoryId){
+    public List<Vacancy> getVacancyByCategory(Long categoryId) {
         // Выборка вакансий по категориям
         return vacancyDao.getVacancyByCategory(categoryId);
     }
 
+    public void createVacancy(VacancyDto vacancyDto) {
+        vacancyDao.createVacancy(Vacancy.builder()
+                .title(vacancyDto.getTitle())
+                .salary(vacancyDto.getSalary())
+                .authorEmail(vacancyDto.getAuthorEmail())
+                .jobDescription(vacancyDto.getJobDescription())
+                .requiredMinExp(vacancyDto.getRequiredMinExp())
+                .requiredMaxExp(vacancyDto.getRequiredMaxExp())
+                .dateOfPosted(vacancyDto.getDateOfPosted())
+                .dateOfUpdated(vacancyDto.getDateOfUpdated())
+                .active(vacancyDto.isActive())
+                .categoryId(vacancyDto.getCategory().getId())
+                .build());
+    }
+
+    public void changeVacancy(VacancyDto vacancyDto) {
+        vacancyDao.changeVacancy(Vacancy.builder()
+                .title(vacancyDto.getTitle())
+                .salary(vacancyDto.getSalary())
+                .authorEmail(vacancyDto.getAuthorEmail())
+                .jobDescription(vacancyDto.getJobDescription())
+                .requiredMinExp(vacancyDto.getRequiredMinExp())
+                .requiredMaxExp(vacancyDto.getRequiredMaxExp())
+                .dateOfPosted(vacancyDto.getDateOfPosted())
+                .dateOfUpdated(vacancyDto.getDateOfUpdated())
+                .active(vacancyDto.isActive())
+                .categoryId(vacancyDto.getCategory().getId())
+                .id(vacancyDto.getId())
+                .build());
+    }
+
+    public void deleteVacancy(Long vacancyId){
+        vacancyDao.deleteVacancy(vacancyId);
+    }
 }
